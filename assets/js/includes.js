b0d285992ab331020e420ee6cb79058ed36dd81d
Gabriel <doemiguel@gmail.com>
2026-03-05 17:00:51 -0300

    mejora general sitio mas fragmentos

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,13 +1,17 @@
 async function loadHTML(id, url) {
-    const res = await fetch(url);
+  // Añadimos un parámetro de tiempo para burlar la caché del navegador
+  const cacheBuster = `?v=${new Date().getTime()}`;
+  const res = await fetch(url + cacheBuster);
+  if (res.ok) {
     document.getElementById(id).innerHTML = await res.text();
+  }
 }
 
 Promise.all([
-    loadHTML("header", "/includes/header.html"),
-    loadHTML("side-menu", "/includes/side-menu.html")
+  loadHTML("header", "/includes/header.html"),
+  loadHTML("side-menu", "/includes/side-menu.html"),
 ]).then(() => {
-    if (window.app && typeof window.app.initMenu === "function") {
-        window.app.initMenu();
-    }
+  if (window.app && typeof window.app.initMenu === "function") {
+    window.app.initMenu();
+  }
 });
